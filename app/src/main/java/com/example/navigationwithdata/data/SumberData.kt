73bccb0c57a343c052769669eda4dc11d9127b5e
Adam Anderson Yuniarto <adam.anderson.ft21@mail.umy.ac.id>
2023-11-06 21:07:23 +0700

    DONE

---
+++ app/src/main/java/com/example/navigationwithdata/data/SumberData.kt
@@ -0,0 +1,13 @@
+package com.example.navigationwithdata.data
+
+import com.example.navigationwithdata.R
+
+object SumberData {
+val flavors = listOf(
+    R.string.flavor1,
+    R.string.flavor2,
+    R.string.flavor3,
+    R.string.flavor4,
+    R.string.flavor5,
+)
+}
