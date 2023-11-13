ffa31d8195e195c26cc3ec167faf75775c71be59
Adam Anderson Yuniarto <adam.anderson.ft21@mail.umy.ac.id>
2023-11-14 01:48:07 +0700

    modifikasi val item (2)

--- app/src/main/java/com/example/navigationwithdata/HalamanDua.kt
+++ app/src/main/java/com/example/navigationwithdata/HalamanDua.kt
@@ -28,8 +28,12 @@ fun HalamanDua(
     modifier: Modifier = Modifier
 ){
     val items = listOf(
+        Pair("Nama Pelanggan", orderUIState.namaPelanggan),
+        Pair("Nomor Telepon", orderUIState.nomorTelepon),
+        Pair("Alamat", orderUIState.alamat),
         Pair("Jumlah", orderUIState.jumlah.toString()),
         Pair("Rasa", orderUIState.rasa),
+        Pair("Harga", orderUIState.harga)
     )
     Column(
         modifier = Modifier,
